d0cca272f11fd5282a2b305f6b86875b8be83297
zoodogood <52154209+zoodogood@users.noreply.github.com>
2021-12-24 21:40:45 +0200

    Update particles.js

--- particles.js
+++ particles.js
@@ -6,8 +6,8 @@ class Scene {
   constructor( querySelector = "canvas" ){
     this.canvas = document.querySelector(querySelector);
     console.log(321);
-    this.canvas.width  = `${ window.innerWidth  }px`;
-    this.canvas.height = `${ window.innerHeight }px`;
+    this.canvas.width  = window.innerWidth;
+    this.canvas.height = window.innerHeight;
     console.log(this.canvas);
     
     this.ctx = this.canvas.getContext("2d");
