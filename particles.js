9f89ed74ff11278face5f2c914835b9ad9384f35
zoodogood <52154209+zoodogood@users.noreply.github.com>
2021-12-25 13:50:32 +0200

    Update particles.js

--- particles.js
+++ particles.js
@@ -5,8 +5,7 @@ const
 class Scene {
   constructor( querySelector = "canvas" ){
     this.canvas = document.querySelector(querySelector);
-    this.canvas.width  = window.innerWidth;
-    this.canvas.height = window.innerHeight;
+    this.#setSize( this.canvas.offsetWidth, this.canvas.offsetHeight );
     
     this.ctx = this.canvas.getContext("2d");
     this.init();
@@ -14,16 +13,28 @@ class Scene {
   
   
   init(){
-    this.#frame();
+    this.#myFrame = this.#frame.bind(this, width, height);
+    this.#myFrame();
   }
   
   
-  #frame(){
+  #setSize(width, height){
+    this.canvas.width  = width;
+    this.canvas.height = height;
+  }
+  
+  
+  #frame(width, height, i = 0){
    let ctx = this.ctx;
    ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-   ctx.rect(40, 40, 70, 70);
+   ctx.rect(40, 40, 70 + i, 70);
    ctx.fill();
-   window.requestAnimationFrame( this.#frame ); 
+   window.requestAnimationFrame( this.#frame, ++i ); 
+  }
+  
+  
+  #resizeHandler(){
+
   }
 }
 
